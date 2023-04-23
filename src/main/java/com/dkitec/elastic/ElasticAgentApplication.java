dea60d9b4dfa7c3d8ffeaadf4f41c463e8d2dd53
dspark <deabal0701@naver.com>
2023-04-23 20:01:28 +0900

    카프카 lag조회, Consumer에서 수신방식 변경 등.

--- src/main/java/com/dkitec/elastic/ElasticAgentApplication.java
+++ src/main/java/com/dkitec/elastic/ElasticAgentApplication.java
@@ -2,12 +2,13 @@ package com.dkitec.elastic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
+@EnableScheduling
 @SpringBootApplication
-public class ElasticAgentTestApplication {
+public class ElasticAgentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ElasticAgentTestApplication.class, args);
+		SpringApplication.run(ElasticAgentApplication.class, args);
 	}
 
 }
--- src/main/java/com/dkitec/elastic/config/ElasticConfig.java
+++ src/main/java/com/dkitec/elastic/config/ElasticConfig.java
@@ -1,4 +1,4 @@
-package com.dkitec.elastic;
+package com.dkitec.elastic.config;
 
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
